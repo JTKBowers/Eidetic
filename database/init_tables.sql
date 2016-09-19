c757d88fc80153f60dea168db90564d6b7dcf5cb
Jamie Bowers <james.t.k.bowers@gmail.com>
2016-09-20 01:30:58 +1200

    Changed type of key hash to be a string (rather than binary).

--- database/init_tables.sql
+++ database/init_tables.sql
@@ -3,7 +3,7 @@ DROP TABLE metrics;
 CREATE TABLE metrics(
   id SERIAL,
   name TEXT PRIMARY KEY,
-  key_hash BYTEA CHECK (length(key_hash) = 128), --the SHA512 hash of the API key for the service.
+  key_hash TEXT CHECK (length(key_hash) = 128), --the SHA512 hash of the API key for the service.
   privileged BOOLEAN --whether authenticaion is required to view
 );
 CREATE TABLE data(
